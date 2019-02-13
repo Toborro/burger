d0c2d88292bb3b7a50130559d7720fb75908ba4b
Toborro <zephyrthewest@gmail.com>
2019-02-12 21:09:19 -0500

    finally committed to committing

---
+++ Eat-Da-Burger!/db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('HamBurglar', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Decker Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tofurger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
